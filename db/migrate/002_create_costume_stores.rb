a1374425b5ded311d8ec855c24512a49cc1adf47
Jesse Barnes <jessekathrynbarnes@gmail.com>
2019-08-05 23:51:24 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -2,7 +2,7 @@
 
 class CreateCostumeStores < ActiveRecord::Migration[5.1]
   def change
-    create_table :costume_store do |t|
+    create_table :costume_stores do |t|
       t.string :name
       t.string :location
       t.string :costume_inventory
