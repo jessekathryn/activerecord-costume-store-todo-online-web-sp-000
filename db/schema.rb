27101a5075ee652d52a0bfe359afadd026368db8
Jesse Barnes <jessekathrynbarnes@gmail.com>
2019-08-05 23:58:14 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -16,16 +16,16 @@ ActiveRecord::Schema.define(version: 3) do
     t.string "name"
     t.string "location"
     t.string "costume_inventory"
-    t.integer "employee_count"
-    t.boolean "business_status"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.integer "num_of_employees"
+    t.boolean "still_in_business"
+    t.datetime "opening_time"
+    t.datetime "closing_time"
   end
 
   create_table "costumes", force: :cascade do |t|
     t.string "name"
     t.float "price"
-    t.string "url"
+    t.string "image_url"
     t.string "size"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -37,8 +37,8 @@ ActiveRecord::Schema.define(version: 3) do
     t.string "theme"
     t.float "price"
     t.boolean "family_friendly"
-    t.datetime "open_date"
-    t.datetime "close_date"
+    t.datetime "opening_date"
+    t.datetime "closing_date"
     t.text "description"
   end
 
