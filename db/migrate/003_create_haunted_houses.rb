a7cd21b17e4b0b6a938f2d083fd7dbf8136939d8
Jesse Barnes <jessekathrynbarnes@gmail.com>
2019-08-05 23:57:01 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -8,8 +8,8 @@ class CreateHauntedHouses < ActiveRecord::Migration[5.1]
       t.string :theme
       t.float :price
       t.boolean :family_friendly
-      t.datetime :open_date
-      t.datetime :close_date
+      t.datetime :opening_date
+      t.datetime :closing_date
       t.text :description
     end
   end
