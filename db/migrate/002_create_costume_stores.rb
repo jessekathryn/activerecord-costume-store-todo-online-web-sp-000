d3a663814a7ec99c1706eb203abdeff42d9142ed
Jesse Barnes <jessekathrynbarnes@gmail.com>
2019-08-05 23:56:21 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -8,9 +8,9 @@ class CreateCostumeStores < ActiveRecord::Migration[5.1]
       t.string :costume_inventory
       t.integer :num_of_employees
       t.boolean :still_in_business
-      t.datetime :open
-      t.datetime :close_date
-      t.timestamps 
+      t.datetime :opening_time
+      t.datetime :closing_time
+      #t.timestamps 
     end
   end
 end
